f075a828c8779d6c07b165f1a33879ada706b390
harinder24 <hss022002@gmail.com>
2023-02-19 19:09:08 -0800

    all done

--- app/src/main/java/com/example/dicerollerkotlin/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerkotlin/MainActivity.kt
@@ -6,17 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
 import com.example.dicerollerkotlin.ui.theme.DiceRollerKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,11 +35,11 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun DiceRollerApp(){
-    Column() {
+
         DiceRollerWithImageAndButton(modifier = Modifier
             .fillMaxSize()
             .wrapContentSize(Alignment.Center))
-    }
+
 
 }
 
@@ -54,9 +56,13 @@ fun DiceRollerWithImageAndButton(modifier: Modifier) {
         5 -> painterResource(id = R.drawable.dice_5)
         else -> painterResource(id = R.drawable.dice_6)
     }
-    Image(painter = image, contentDescription = null)
-
-    Button(onClick = {result = (1..6).random()}) {
+    Column(modifier = modifier
+        ,horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = image, contentDescription = result.toString())
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random()}) {
             Text(text = stringResource(R.string.roll))
+        }
     }
+
 }
